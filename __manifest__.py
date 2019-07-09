c82977bd44d6784ea60d45f1ee681d637e859016
Ioan CHIRIAC <i.chiriac@yahoo.com>
2019-07-09 10:51:12 +0200

    up info

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "f2f_test_module",
+    'name': "Module de test",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,14 +10,15 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "F2F",
+    'website': "https://www.f2f.fr",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Attendance',
+    'version': '0.2',
+    'application': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
